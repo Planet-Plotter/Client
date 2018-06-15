1162f7df52b6e305851ebf1b84b4c5943a12d96f
SethDonohue <donohue.seth@gmail.com>
2018-06-15 15:02:35 -0700

    added index.js for reverse proxy starting

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const path = require('path'); //eslint-disable-line
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/', function (req, res) { //eslint-disable-line
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.listen(9000);
