db2c97b4f6db0384b9a31bf7f77639aa59925f99
SethDonohue <donohue.seth@gmail.com>
2018-06-15 17:13:29 -0700

    corrected index to server from th correct path

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const express = require('express');
 const path = require('path'); //eslint-disable-line
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'build')));
+//make sure this matches the path from the homepage key in package.json
+app.use('/apod-gallery/', express.static(path.join(__dirname, 'build')));
 
 app.get('/*', function (req, res) { //eslint-disable-line
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
